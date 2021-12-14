06bfc7296a6991b67c96eca2a67f85274a5705b1
zhangjf <1061683512@qq.com>
2021-12-14 11:44:29 +0800

    Create prepare_data.py

---
+++ data/prepare_data.py
@@ -0,0 +1,23 @@
+import os
+if not os.path.exists("MLHomework_Toxicity/train_origin.csv"):
+    if os.path.exists("MLHomework_Toxicity"):
+        os.system("rm -rf MLHomework_Toxicity")
+    if not os.path.exists("MLHomework_Toxicity.zip"):
+        print("Downloading MLHomework_Toxicity.zip ...")
+        os.system("wget https://drive.google.com/file/d/1nhh2G8pMLxqMxxHK2yzX_0lH-24pNAqp/view?usp=sharing")
+    os.system("unzip MLHomework_Toxicity.zip")
+    os.system("mv MLHomework_Toxicity/train.csv MLHomework_Toxicity/train_origin.csv")
+
+import pandas as pd
+import numpy as np
+file_path = "MLHomework_Toxicity/train_origin.csv"
+file = pd.read_csv(file_path)
+df = pd.DataFrame(file)
+
+test_index = np.random.choice(range(df.shape[0]), int(df.shape[0] * 0.1), replace=False)
+df_test = df.loc[sorted(test_index)]
+df_test.to_csv("MLHomework_Toxicity/test.csv")
+
+train_index = [i for i in range(df.shape[0]) if i not in test_index]
+df_train = df.loc[sorted(train_index)]
+df_train.to_csv("MLHomework_Toxicity/train.csv")
