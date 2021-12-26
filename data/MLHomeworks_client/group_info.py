e5c935ddff1d2d2c09bb34cd02a8832b6c294f54
zhangjf-nlp <1061683512@qq.com>
2021-12-26 15:18:32 +0800

    add_client

---
+++ data/MLHomeworks_client/group_info.py
@@ -0,0 +1,5 @@
+# Your should modified these information if you want to registry an account and submit your results. 
+SID = "TEAM_ID"
+TOKEN = "TEAM_TOKEN"
+NAME = "GROUP_NAME"
+EMAIL = "GROUP_EMAIL"
